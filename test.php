65fece5fdb2a8d3a40e4dcecc63eb4e09d017e49
markie <markie2902@yahoo.com>
2015-03-26 21:18:52 -0700

    ya, whatever

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+echo "<html>";
+echo "<body>";
+echo "I am inside the body tag<br />";
+for($i = 0; $i < 10; $i++){
+	echo "Counting $i <br />";
+}
+echo "</body>";
+echo "</html>";
+//echo phpinfo();
+echo("I am echoing");
+?>
